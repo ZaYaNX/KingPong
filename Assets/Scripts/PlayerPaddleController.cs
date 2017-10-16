482f72970ab1c682c9d01d1436e364a3968a423b
Guillaume Secret <g.secret91@gmail.com>
2017-10-16 17:50:27 +0200

    First commit

---
+++ Assets/Scripts/PlayerPaddleController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerPaddleController : MonoBehaviour {
+	public int Speed = 10;
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * Speed,0,0);
+		this.transform.Translate(movement * Time.deltaTime);
+	}
+}
